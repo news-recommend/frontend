a3f2eba2477ea430ec8bbcf97c5924581da29c81
mayrang <pkss0626@naver.com>
2025-06-04 04:09:30 +0900

    Fix: 다른식으로 받기

--- src/app/profile/_hooks/useProfileEdit.tsx
+++ src/app/profile/_hooks/useProfileEdit.tsx
@@ -60,7 +60,7 @@ export default function useProfileEditForm() {
         name: data?.name ?? "",
         gender: data?.gender ?? "",
         ageGroup: data?.agegroup ?? "",
-        preferredTags: data?.interestCategory ?? [],
+        preferredTags: data?.interestCategory?.split(",") ?? [],
       });
     }
   }, [isSuccess, isLoading]);
--- src/model/user.ts
+++ src/model/user.ts
@@ -5,5 +5,5 @@ export type Profile = {
   gender: "남성" | "여성";
   agegroup: "10대" | "20대" | "30대" | "40대" | "50대" | "60대 이상";
   preferredTags: string[];
-  interestCategory?: string[];
+  interestCategory?: string;
 };
